afaa5d1b2a65e36509440b66b2fe68c62c8fe093
natepichler <92587036+natepichler@users.noreply.github.com>
2022-09-02 22:45:34 -0500

    Update and rename bmetest.py to bmetestiaq.py
    added bme.aqi

--- bmetestiaq.py
+++ bmetestiaq.py
@@ -1,10 +1,11 @@
 from bme680 import *
 from machine import I2C, Pin
+import math
 import time
 bme = BME680_I2C(I2C(-1, Pin(13), Pin(12)))
 
 for _ in range(3):
-    print(bme.temperature, bme.humidity, bme.pressure, bme.gas)
+    print(bme.temperature, bme.humidity, bme.pressure, bme.gas, bme.aqi)
     time.sleep(1)
 
 
