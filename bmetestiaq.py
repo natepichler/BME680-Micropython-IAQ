e4f8f7c8a1d964a44f6689e293b574154ad6b56b
natepichler <92587036+natepichler@users.noreply.github.com>
2022-09-03 11:28:17 -0500

    Update bmetestiaq.py

--- bmetestiaq.py
+++ bmetestiaq.py
@@ -1,11 +1,27 @@
-from bme680 import *
+from bme680iaq import *
 from machine import I2C, Pin
 import math
 import time
-bme = BME680_I2C(I2C(-1, Pin(13), Pin(12)))
+i2c = I2C(0, scl=Pin(13), sda=Pin(12))
+bme = BME680_I2C(i2c=i2c)
 
-for _ in range(3):
-    print(bme.temperature, bme.humidity, bme.pressure, bme.gas, bme.aqi)
-    time.sleep(1)
+gas_max=50000
+
+for _ in range(5):
+    print(bme.comp_gas)
+    time.sleep(3)
+    
+while True:
+    if bme.comp_gas > gas_max:
+        gas_max = bme.comp_gas
+            
+    AQ = min((bme.comp_gas/gas_max)**2,1)*100
+    IAQ = (1 - (AQ/100))*500
+    print(bme.temperature, bme.humidity, bme.pressure, bme.comp_gas, IAQ)
+    time.sleep(3)
+
+
+
+
 
 
